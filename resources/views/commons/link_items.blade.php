6f476bfda5ee634b324f78a34d3143beb644c9da
tomoya.suzuki <cnoeko@gmail.com>
2023-04-13 15:39:00 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{-- ユーザ一覧ページへのリンク --}}
+    <li><a class="link link-hover" href="#">Users</a></li>
+    {{-- ユーザ詳細ページへのリンク --}}
+    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">新規登録</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
+@endif
